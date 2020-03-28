9961e8e8bcc2ad421c2dc1c1cb3ac935120a528a
Oyinloluwa Balogun <OBalogun@gcn.ab-inbev.com>
2020-03-28 13:05:56 -0700

    Almost working

--- INBEV/request-app/src/Components/Profiles/Dashboard.js
+++ INBEV/request-app/src/Components/Profiles/Dashboard.js
@@ -1,66 +1,193 @@
-import React, { Component } from 'react';
-import { Button, Progress } from 'semantic-ui-react'
-import Merchtable from './Merchtable';
+import React, { Component } from "react";
+import { Button, Progress } from "semantic-ui-react";
+import Merchtable from "./Merchtable";
 
 class Dashboard extends Component {
-	state = {
-		percent: 33,
-		products: [
-			{ name: 'Budweiser', mlone: '375ml', mltwo: '600ml', mlthree: '330ml', mlfour: '500ml', mlfive: '-', mlsix: '-', value: '0', value2: '0', key: '1' },
-			{ name: 'Castle Lite', mlone: '375ml', mltwo: '600ml', mlthree: '-', mlfour: '-', mlfive: '-', mlsix: '-', value: '0', value2: '0', key: '2' },
-			{ name: 'Beta Malt', mlone: '330ml', mltwo: '600ml', mlthree: '330ml', mlfour: '-', mlfive: '250ml', mlsix: '330ml', value: '0', value2: '0', key: '3' },
-			{ name: 'Hero', mlone: '375ml', mltwo: '600ml', mlthree: '330ml', mlfour: '500ml', mlfive: '-', mlsix: '-', value: '0', value2: '0', key: '4' },
-			{ name: 'Grand Malt', mlone: '375ml', mltwo: '600ml', mlthree: '330ml', mlfour: '-', mlfive: '250ml', mlsix: '330ml', value: '0', value2: '0', key: '5' },
-			{ name: 'Trophy Stout', mlone: '-', mltwo: '600ml', mlthree: '-', mlfour: '-', mlfive: '-', mlsix: '-', value: '0', value2: '0', key: '6' },
-			{ name: 'Trophy', mlone: '375ml', mltwo: '600ml', mlthree: '330ml', mlfour: '500ml', mlfive: '-', mlsix: '-', value: '0', value2: '0', key: '7' },
-			{ name: 'Eagle Lager', mlone: '330ml', mltwo: '600ml', mlthree: '-', mlfour: '-', mlfive: '-', mlsix: '-', value: '0', value2: '0', key: '8' },
-			{ name: 'Eagle Stout', mlone: '330ml', mltwo: '600ml', mlthree: '-', mlfour: '-', mlfive: '-', mlsix: '-', value: '0', value2: '0', key: '9' },
-		]
-	}
-	setUpdate = (value, name, key) => {
-		const products = this.state.products;
-		products.map(prod => {
-			if (prod.key === key) {
-				if (name === "value") {
-					prod.value = value;
-				} else {
-					prod.value2 = value;
-				}
-			}
-		})
-		this.setState({
-			products: products
-		})
-	}
-	increment = () =>
-		this.setState((prevState) => ({
-			percent: prevState.percent >= 100 ? 0 : prevState.percent + 20,
-		}))
+  state = {
+    percent: 33,
+    products: [
+      {
+        name: "Budweiser",
+        mlone: "375ml (rgb)",
+        mltwo: "600ml (rgb)",
+        mlthree: "330ml (can)",
+        mlfour: "500ml (can)",
+        mlfive: "-",
+        mlsix: "-",
+        value: "0",
+		value2: "0",
+		value3: '0',
+		value4: '0',
+		value5: '0',
+		value6: '0',
+        key: "1"
+      },
+      {
+        name: "Castle Lite",
+        mlone: "375ml (rgb)",
+        mltwo: "600ml (rgb)",
+        mlthree: "-",
+        mlfour: "-",
+        mlfive: "-",
+        mlsix: "-",
+        value: "0",
+		value2: "0",
+		value3: '0',
+		value4: '0',
+		value5: '0',
+		value6: '0',
+        key: "2"
+      },
+      {
+        name: "Beta Malt",
+        mlone: "330ml (rgb)",
+        mltwo: "600ml (rgb)",
+        mlthree: "330ml (can)",
+        mlfour: "-",
+        mlfive: "250ml (pet)",
+        mlsix: "330ml (pet)",
+        value: "0",
+		value2: "0",
+		value3: '0',
+		value4: '0',
+		value5: '0',
+		value6: '0',
+        key: "3"
+      },
+      {
+        name: "Hero",
+        mlone: "375ml (rgb)",
+        mltwo: "600ml (rgb)",
+        mlthree: "330ml (can)",
+        mlfour: "500ml (can)",
+        mlfive: "-",
+        mlsix: "-",
+        value: "0",
+		value2: "0",
+		value3: '0',
+		value4: '0',
+		value5: '0',
+		value6: '0',
+        key: "4"
+      },
+      {
+        name: "Grand Malt",
+        mlone: "375ml (rgb)",
+        mltwo: "600ml (rgb)",
+        mlthree: "330ml (can)",
+        mlfour: "-",
+        mlfive: "250ml (pet)",
+        mlsix: "330ml (pet)",
+        value: "0",
+		value2: "0",
+		value3: '0',
+		value4: '0',
+		value5: '0',
+		value6: '0',
+        key: "5"
+      },
+      {
+        name: "Trophy Stout",
+        mlone: "-",
+        mltwo: "600ml (rgb)",
+        mlthree: "-",
+        mlfour: "-",
+        mlfive: "-",
+        mlsix: "-",
+        value: "0",
+		value2: "0",
+		value3: '0',
+		value4: '0',
+		value5: '0',
+		value6: '0',
+        key: "6"
+      },
+      {
+        name: "Trophy",
+        mlone: "375ml (rgb)",
+        mltwo: "600ml (rgb)",
+        mlthree: "330ml (can)",
+        mlfour: "500ml (can)",
+        mlfive: "-",
+        mlsix: "-",
+        value: "0",
+        value2: "0",
+        key: "7"
+      },
+      {
+        name: "Eagle Lager",
+        mlone: "330ml (rgb)",
+        mltwo: "600ml (rgb)",
+        mlthree: "-",
+        mlfour: "-",
+        mlfive: "-",
+        mlsix: "-",
+        value: "0",
+        value2: "0",
+        key: "8"
+      },
+      {
+        name: "Eagle Stout",
+        mlone: "330ml (rgb)",
+        mltwo: "600ml (rgb)",
+        mlthree: "-",
+        mlfour: "-",
+        mlfive: "-",
+        mlsix: "-",
+        value: "0",
+        value2: "0",
+        key: "9"
+      }
+    ]
+  };
+  setUpdate = (value, name, key) => {
+    const products = this.state.products;
+    products.map(prod => {
+      if (prod.key === key) {
+        if (name === "value") {
+          prod.value = value;
+        } else {
+          prod.value2 = value;
+          prod.value3 = value;
+          prod.value4 = value;
+          prod.value5 = value;
+          prod.value6 = value;
+        }
+      }
+    });
+    this.setState({
+      products: products
+    });
+  };
+  increment = () =>
+    this.setState(prevState => ({
+      percent: prevState.percent >= 100 ? 0 : prevState.percent + 20
+    }));
 
-	render() {
-		return (
-			<div class='ui container' id="container">
-				<div id="content-container">
-					<div id="stockvalues">
-						<h6>STOCK VALUES IN CRATES</h6>
-						<p>Click value to edit</p>
-						<div id="merchtable">
-							<Merchtable
-								products={this.state.products}
-								setUpdate={this.setUpdate}
-							/>
-						</div>
-					</div>
-					<div id="bar">
-						<h6>Latest request status</h6>
-						<div id="progress-bar">
-							<Progress id="progress" percent={this.state.percent} indicating />
-						</div>
-					</div>
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div class="ui container" id="container">
+        <div id="content-container">
+          <div id="stockvalues">
+            <h6>STOCK VALUES IN CRATES</h6>
+            <p>Click value to edit</p>
+            <div id="merchtable">
+              <Merchtable
+                products={this.state.products}
+                setUpdate={this.setUpdate}
+              />
+            </div>
+          </div>
+          <div id="bar">
+            <h6>Latest request status</h6>
+            <div id="progress-bar">
+              <Progress id="progress" percent={this.state.percent} indicating />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
--- INBEV/request-app/src/Components/Profiles/Merchtable.js
+++ INBEV/request-app/src/Components/Profiles/Merchtable.js
@@ -1,88 +1,138 @@
-import React from 'react'
-import { Header, Table, Icon } from 'semantic-ui-react'
-
+import React from "react";
+import { Header, Table, Icon } from "semantic-ui-react";
 
 export default function Merchtable(props) {
-	return (
-		<Table celled structured id="stocktable">
-			<Table.Header>
-				<Table.Row>
-					<Table.HeaderCell rowSpan='2'>Brand</Table.HeaderCell>
-					<Table.HeaderCell rowSpan='2'>RGB</Table.HeaderCell>
-					<Table.HeaderCell rowSpan='2'>CAN</Table.HeaderCell>
-					<Table.HeaderCell rowSpan='2'>PET</Table.HeaderCell>
-					<Table.HeaderCell rowSpan='2'>Value</Table.HeaderCell>
-				</Table.Row>
-			</Table.Header>
+  return (
+    <Table celled structured id="stocktable">
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell rowSpan="2">Brand</Table.HeaderCell>
+          <Table.HeaderCell rowSpan="2">RGB/CAN/PET</Table.HeaderCell>
+          <Table.HeaderCell rowSpan="2">Value</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
 
-			{props.products.map(prod => (
-				<Table.Body>
-					<Table.Row>
-						<Table.Cell rowSpan='2'>{prod.name}</Table.Cell>
-						<Table.Cell>{prod.mlone}</Table.Cell>
-						<Table.Cell>{prod.mlthree}</Table.Cell>
-						<Table.Cell>{prod.mlfive}</Table.Cell>
-						<Table.Cell className="dashboard-value">
-							<input type="text"
-								id={prod.key}
-								value={prod.value}
-								name="value"
-								onChange={(e) => {
-									props.setUpdate(e.target.value, e.target.name, prod.key)
-								}} />
-						</Table.Cell>
-					</Table.Row>
-					<Table.Row>
-						<Table.Cell>{prod.mltwo}</Table.Cell>
-						<Table.Cell>{prod.mlfour}</Table.Cell>
-						<Table.Cell>{prod.mlsix}</Table.Cell>
-						<Table.Cell className="dashboard-value">
-							<input type="text"
-								id={prod.key}
-								value={prod.value2}
-								name="value2"
-								onChange={(e) => {
-									props.setUpdate(e.target.value, e.target.name, prod.key)
-								}} />
-						</Table.Cell>
-					</Table.Row>
-
-				</Table.Body>
-			))}
-
-		</Table>
-	)
+      {props.products.map(prod => (
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell rowSpan="6">{prod.name}</Table.Cell>
+            <Table.Cell>{prod.mlone}</Table.Cell>
+            <Table.Cell className="dashboard-value">
+              <input
+                type="text"
+                id={prod.key}
+                value={prod.value}
+                name="value"
+                onChange={e => {
+                  props.setUpdate(e.target.value, e.target.name, prod.key);
+                }}
+              />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>{prod.mltwo}</Table.Cell>
+            <Table.Cell className="dashboard-value">
+              <input
+                type="text"
+                id={prod.key}
+                value={prod.value2}
+                name="value2"
+                onChange={e => {
+                  props.setUpdate(e.target.value, e.target.name, prod.key);
+                }}
+              />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>{prod.mlthree}</Table.Cell>
+            <Table.Cell className="dashboard-value">
+              <input
+                type="text"
+                id={prod.key}
+                value={prod.value3}
+                name="value3"
+                onChange={e => {
+                  props.setUpdate(e.target.value, e.target.name, prod.key);
+                }}
+              />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>{prod.mlfour}</Table.Cell>
+            <Table.Cell className="dashboard-value">
+              <input
+                type="text"
+                id={prod.key}
+                value={prod.value4}
+                name="value4"
+                onChange={e => {
+                  props.setUpdate(e.target.value, e.target.name, prod.key);
+                }}
+              />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>{prod.mlfive}</Table.Cell>
+            <Table.Cell className="dashboard-value">
+              <input
+                type="text"
+                id={prod.key}
+                value={prod.value5}
+                name="value5"
+                onChange={e => {
+                  props.setUpdate(e.target.value, e.target.name, prod.key);
+                }}
+              />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>{prod.mlsix}</Table.Cell>
+            <Table.Cell className="dashboard-value">
+              <input
+                type="text"
+                id={prod.key}
+                value={prod.value6}
+                name="value6"
+                onChange={e => {
+                  props.setUpdate(e.target.value, e.target.name, prod.key);
+                }}
+              />
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      ))}
+    </Table>
+  );
 }
 
+// <Table basic='very' celled collapsing>
+//     <Table.Header>
+//         <Table.Row>
+//             <Table.HeaderCell>Product</Table.HeaderCell>
+//             <Table.HeaderCell>Stock Level</Table.HeaderCell>
+//         </Table.Row>
+//     </Table.Header>
 
-            // <Table basic='very' celled collapsing>
-        //     <Table.Header>
-        //         <Table.Row>
-        //             <Table.HeaderCell>Product</Table.HeaderCell>
-        //             <Table.HeaderCell>Stock Level</Table.HeaderCell>
-        //         </Table.Row>
-        //     </Table.Header>
+//     <Table.Body>
+//         {props.products.map(prod => (
+//             <Table.Row>
+//                 <Table.Cell key={prod.name}>
+//                     <Header as='h4'>
+//                         <Header.Content>
+//                             {prod.name}
+//                         </Header.Content>
+//                     </Header>
+//                 </Table.Cell>
+//                 <Table.Cell className="dashboard-value">
+//                     <input type="text"
+//                     id={prod.key}
+//                     value={prod.value}
+//                     onChange={(e) => {
+//                         props.setUpdate(e.target.value, prod.key)
+//                     }} />
+//                 </Table.Cell>
+//             </Table.Row>
+//         ))}
 
-        //     <Table.Body>
-        //         {props.products.map(prod => (
-        //             <Table.Row>
-        //                 <Table.Cell key={prod.name}>
-        //                     <Header as='h4'>
-        //                         <Header.Content>
-        //                             {prod.name}
-        //                         </Header.Content>
-        //                     </Header>
-        //                 </Table.Cell>
-        //                 <Table.Cell className="dashboard-value">
-        //                     <input type="text" 
-        //                     id={prod.key} 
-        //                     value={prod.value} 
-        //                     onChange={(e) => {
-        //                         props.setUpdate(e.target.value, prod.key)
-        //                     }} />
-        //                 </Table.Cell>
-        //             </Table.Row>
-        //         ))}
-
-        //     </Table.Body>
-        // </Table>
+//     </Table.Body>
+// </Table>
