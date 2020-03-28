189eb794e297a0be2fda3760ad5f8b4d40d70aa2
Joseph <dabigjoe6@gmail.com>
2020-03-28 20:29:40 +0100

    Rough path for the value stuff

--- INBEV/request-app/src/Components/Profiles/Merchtable.js
+++ INBEV/request-app/src/Components/Profiles/Merchtable.js
@@ -3,55 +3,55 @@ import { Header, Table, Icon } from 'semantic-ui-react'
 
 
 export default function Merchtable(props) {
-    return (
-        <Table celled structured id="stocktable">
-            <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell rowSpan='2'>Brand</Table.HeaderCell>
-                    <Table.HeaderCell rowSpan='2'>RGB</Table.HeaderCell>
-                    <Table.HeaderCell rowSpan='2'>CAN</Table.HeaderCell>
-                    <Table.HeaderCell rowSpan='2'>PET</Table.HeaderCell>
-                    <Table.HeaderCell rowSpan='2'>Value</Table.HeaderCell>
-                </Table.Row>
-            </Table.Header>
+	return (
+		<Table celled structured id="stocktable">
+			<Table.Header>
+				<Table.Row>
+					<Table.HeaderCell rowSpan='2'>Brand</Table.HeaderCell>
+					<Table.HeaderCell rowSpan='2'>RGB</Table.HeaderCell>
+					<Table.HeaderCell rowSpan='2'>CAN</Table.HeaderCell>
+					<Table.HeaderCell rowSpan='2'>PET</Table.HeaderCell>
+					<Table.HeaderCell rowSpan='2'>Value</Table.HeaderCell>
+				</Table.Row>
+			</Table.Header>
 
-            {props.products.map(prod => (
-                <Table.Body>
-                    <Table.Row>
-                        <Table.Cell rowSpan='2'>{prod.name}</Table.Cell>
-                        <Table.Cell>{prod.mlone}</Table.Cell>
-                        <Table.Cell>{prod.mlthree}</Table.Cell>
-                        <Table.Cell>{prod.mlfive}</Table.Cell>
-                        <Table.Cell className="dashboard-value">
-                            <input type="text"
-                                id={prod.key}
-                                value={prod.value}
-                                
-                                onChange={(e) => {
-                                    props.setUpdate(e.target.value, prod.key)
-                                }} />
-                        </Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                        <Table.Cell>{prod.mltwo}</Table.Cell>
-                        <Table.Cell>{prod.mlfour}</Table.Cell>
-                        <Table.Cell>{prod.mlsix}</Table.Cell>
-                        <Table.Cell className="dashboard-value">
-                            <input type="text"
-                                id={prod.key}
-                                value={prod.value}
-                                
-                                onChange={(e) => {
-                                    props.setUpdate(e.target.value, prod.key)
-                                }} />
-                        </Table.Cell>
-                    </Table.Row>
+			{props.products.map(prod => (
+				<Table.Body>
+					<Table.Row>
+						<Table.Cell rowSpan='2'>{prod.name}</Table.Cell>
+						<Table.Cell>{prod.mlone}</Table.Cell>
+						<Table.Cell>{prod.mlthree}</Table.Cell>
+						<Table.Cell>{prod.mlfive}</Table.Cell>
+						<Table.Cell className="dashboard-value">
+							<input type="text"
+								id={prod.key}
+								value={prod.value}
+								name="value"
+								onChange={(e) => {
+									props.setUpdate(e.target.value, e.target.name, prod.key)
+								}} />
+						</Table.Cell>
+					</Table.Row>
+					<Table.Row>
+						<Table.Cell>{prod.mltwo}</Table.Cell>
+						<Table.Cell>{prod.mlfour}</Table.Cell>
+						<Table.Cell>{prod.mlsix}</Table.Cell>
+						<Table.Cell className="dashboard-value">
+							<input type="text"
+								id={prod.key}
+								value={prod.value2}
+								name="value2"
+								onChange={(e) => {
+									props.setUpdate(e.target.value, e.target.name, prod.key)
+								}} />
+						</Table.Cell>
+					</Table.Row>
 
-                </Table.Body>
-            ))}
+				</Table.Body>
+			))}
 
-        </Table>
-    )
+		</Table>
+	)
 }
 
 
